1db6babccb6fbc82a32134b1a107f138d516d15d
Akir4 <160866779+AKIRA-NZR@users.noreply.github.com>
2025-11-08 18:57:53 -0300

    Update Cliente.java

--- Projeto_java_Construtora/Codigofonte_construtora/src/PessoalEnvolvido/Cliente.java
+++ Projeto_java_Construtora/Codigofonte_construtora/src/PessoalEnvolvido/Cliente.java
@@ -1,5 +1,17 @@
 package PessoalEnvolvido;
 
-public class Cliente {
+public class Cliente extends Pessoa {
+    private String email;
+    private double rendaMensal;
     
+public Cliente(String nome, String cpf,String telefone, String endereco, String email, double rendaMensal){
+    super(nome,cpf,telefone,endereco);
+    this.rendaMensal = rendaMensal;
+    this.email = email;
+}
+public Cliente(String nome, String email, double rendaMensal){
+    super(nome);
+    this.rendaMensal = rendaMensal;
+    this.email = email;
+}
 }
